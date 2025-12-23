6e3c56bdf4464892d1628ef0e831695fbbb6e374
Marian Kleineberg <mail@marian42.de>
2025-12-23 12:20:25 +0000

    Implement London Underground clock

--- myfirstproject/src/c/myfirstproject.c
+++ myfirstproject/src/c/myfirstproject.c
@@ -1,60 +1,247 @@
 #include <pebble.h>
 
-static Window *s_window;
-static TextLayer *s_text_layer;
+static Window *s_main_window;
+static Layer *s_canvas_layer;
 
-static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Select");
+static struct tm s_last_time;
+
+// Blocks
+// 0: empty
+// 1: square
+// 2: round, top left
+// 3: round, top right
+// 4: round, bottom right
+// 5: round, bottom left
+// 6: diagonal, top left
+// 7: diagonal, top right
+// 8: diagonal, bottom right
+// 9: diagonal, bottom left
+// 10: notch, left
+// 11: notch, right
+
+static const uint16_t DIGITS[10][15]  = {
+    { 2, 1, 3, 1, 0, 1, 1, 0, 1, 1, 0, 1, 5, 1, 4 }, // 0
+    { 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1 }, // 1
+    { 2, 1, 3, 0, 0, 1, 6, 1, 8, 1, 0, 0, 1, 1, 1 }, // 2
+    { 2, 1, 3, 0, 0, 1, 0, 1, 11, 0, 0, 1, 5, 1, 4 }, // 3
+    { 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1 }, // 4
+    { 1, 1, 1, 1, 0, 0, 1, 1, 7, 0, 0, 1, 5, 1, 4 }, // 5
+    { 2, 1, 3, 1, 0, 0, 1, 1, 7, 1, 0, 1, 5, 1, 4 }, // 6
+    { 1, 1, 1, 0, 6, 8, 6, 8, 0, 1, 0, 0, 1, 0, 0 }, // 7
+    { 2, 1, 3, 1, 0, 1, 10, 1, 11, 1, 0, 1, 5, 1, 4 }, // 8
+    { 2, 1, 3, 1, 0, 1, 9, 1, 1, 0, 0, 1, 5, 1, 4 } // 9
+};
+
+static void draw_block(GContext *ctx, uint16_t x, uint16_t y, uint16_t size, uint16_t block) {
+    if (block == 0) {
+        return;
+    }
+    if (block == 1) {        
+        graphics_fill_rect(ctx, GRect(x, y, size, size), 0, GCornerNone);
+        return;
+    }
+    uint16_t size_s = size - 1;
+    uint16_t threshold = size_s * size_s + 10;
+    if (block == 2) {
+        for (uint16_t px = 0; px < size; px++) {
+            for (uint16_t py = 0; py < size; py++) {
+                if ((size_s - px) * (size_s - px) + (size_s - py) * (size_s - py) <= threshold) {
+                    graphics_draw_pixel(ctx, GPoint(x + px, y + py));
+                }
+            }
+        }
+        return;
+    }
+    if (block == 3) {
+        for (uint16_t px = 0; px < size; px++) {
+            for (uint16_t py = 0; py < size; py++) {
+                if (px * px + (size_s - py) * (size_s - py) <= threshold) {
+                    graphics_draw_pixel(ctx, GPoint(x + px, y + py));
+                }
+            }
+        }
+        return;
+    }
+    if (block == 4) {
+        for (uint16_t px = 0; px < size; px++) {
+            for (uint16_t py = 0; py < size; py++) {
+                if (px * px + py * py <= threshold) {
+                    graphics_draw_pixel(ctx, GPoint(x + px, y + py));
+                }
+            }
+        }
+        return;
+    }
+    if (block == 5) {
+        for (uint16_t px = 0; px < size; px++) {
+            for (uint16_t py = 0; py < size; py++) {
+                if ((size_s - px) * (size_s - px) + py * py <= threshold) {
+                    graphics_draw_pixel(ctx, GPoint(x + px, y + py));
+                }
+            }
+        }
+        return;
+    }
+    if (block == 6) {
+        for (uint16_t px = 0; px < size; px++) {
+            for (uint16_t py = 0; py < size; py++) {
+                if (px + py >= size - 1) {
+                    graphics_draw_pixel(ctx, GPoint(x + px, y + py));
+                }
+            }
+        }
+        return;
+    }
+    if (block == 7) {
+        for (uint16_t px = 0; px < size; px++) {
+            for (uint16_t py = 0; py < size; py++) {
+                if (px - py <= 0) {
+                    graphics_draw_pixel(ctx, GPoint(x + px, y + py));
+                }
+            }
+        }
+        return;
+    }
+    if (block == 8) {
+        for (uint16_t px = 0; px < size; px++) {
+            for (uint16_t py = 0; py < size; py++) {
+                if (px + py < size) {
+                    graphics_draw_pixel(ctx, GPoint(x + px, y + py));
+                }
+            }
+        }
+        return;
+    }
+    if (block == 9) {
+        for (uint16_t px = 0; px < size; px++) {
+            for (uint16_t py = 0; py < size; py++) {
+                if (py - px <= 0) {
+                    graphics_draw_pixel(ctx, GPoint(x + px, y + py));
+                }
+            }
+        }
+        return;
+    }
+    if (block == 10) {
+        for (uint16_t px = 0; px < size; px++) {
+            for (uint16_t py = 0; py < size; py++) {
+                if (px + py >= size - 1 || py - px <= 0) {
+                    graphics_draw_pixel(ctx, GPoint(x + px, y + py));
+                }
+            }
+        }
+        return;
+    }
+    if (block == 11) {
+        for (uint16_t px = 0; px < size; px++) {
+            for (uint16_t py = 0; py < size; py++) {
+                if (px - py <= 0 || px + py < size) {
+                    graphics_draw_pixel(ctx, GPoint(x + px, y + py));
+                }
+            }
+        }
+        return;
+    }
+
+    graphics_fill_rect(ctx, GRect(x, y, size, size), 0, GCornerNone);
 }
 
-static void prv_up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Up");
+static void draw_digit(GContext *ctx, uint16_t x, uint16_t y, uint16_t size, uint16_t digit) {
+    for (uint16_t i = 0; i < 15; i++) {
+        uint16_t local_x = x + (i % 3) * (size + 1);
+        uint16_t local_y = y + (i / 3) * (size + 1);
+        uint16_t block_value = DIGITS[digit][i];
+        draw_block(ctx, local_x, local_y, size, block_value);
+    }
 }
 
-static void prv_down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Down");
+static void canvas_update_proc(Layer *layer, GContext *ctx)
+{
+    // Fill background black
+
+    GRect bounds = layer_get_bounds(layer);
+
+    graphics_context_set_fill_color(ctx, GColorBlack);
+    graphics_fill_rect(ctx, bounds, 0, GCornerNone);
+
+    
+    graphics_context_set_fill_color(ctx, GColorWhite);
+    graphics_context_set_stroke_color(ctx, GColorWhite);
+
+    const bool show_seconds = true;
+
+    const uint16_t gap = 3;
+    const uint16_t minute_second_gap = 3;
+    uint16_t size;
+    uint16_t x_position;
+    if (show_seconds) {
+        size = (bounds.size.w - gap * 5) * 5 / 78 - 1;
+        x_position = (bounds.size.w - (size + 1) * 78 / 5 - gap * 5 - minute_second_gap) / 2;
+    } else {
+        size = (bounds.size.w - gap * 3) / 12 - 1;
+        x_position = (bounds.size.w - (size + 1) * 12 - gap * 3) / 2;
+    }
+    uint16_t spacing = (size + 1) * 3 + gap;
+    uint16_t y_position = (bounds.size.h - (size + 1) * 5) / 2;
+
+    draw_digit(ctx, x_position + spacing * 0, y_position, size, s_last_time.tm_hour / 10);
+    draw_digit(ctx, x_position + spacing * 1, y_position, size, s_last_time.tm_hour % 10);
+    draw_digit(ctx, x_position + spacing * 2, y_position, size, s_last_time.tm_min / 10);
+    draw_digit(ctx, x_position + spacing * 3, y_position, size, s_last_time.tm_min % 10);
+
+    if (show_seconds) {
+        uint16_t seconds_size = size * 3 / 5;
+        x_position = x_position + spacing * 4 + minute_second_gap;
+        y_position = (bounds.size.h + (size + 1) * 5) / 2 - (seconds_size + 1) * 5;
+        spacing = (seconds_size + 1) * 3 + gap;
+        draw_digit(ctx, x_position + spacing * 0, y_position, seconds_size, s_last_time.tm_sec / 10);
+        draw_digit(ctx, x_position + spacing * 1, y_position, seconds_size, s_last_time.tm_sec % 10);
+    }
 }
 
-static void prv_click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, prv_select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, prv_up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, prv_down_click_handler);
+static void main_window_load(Window *window)
+{
+    Layer *window_layer = window_get_root_layer(window);
+    GRect bounds = layer_get_bounds(window_layer);
+
+    s_canvas_layer = layer_create(bounds);
+    layer_set_update_proc(s_canvas_layer, canvas_update_proc);
+    layer_add_child(window_layer, s_canvas_layer);
 }
 
-static void prv_window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
-
-  s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_text(s_text_layer, "Press a button");
-  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+static void main_window_unload(Window *window)
+{
+    layer_destroy(s_canvas_layer);
 }
 
-static void prv_window_unload(Window *window) {
-  text_layer_destroy(s_text_layer);
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
+{
+    s_last_time = *tick_time;
+    layer_mark_dirty(s_canvas_layer);
 }
 
-static void prv_init(void) {
-  s_window = window_create();
-  window_set_click_config_provider(s_window, prv_click_config_provider);
-  window_set_window_handlers(s_window, (WindowHandlers) {
-    .load = prv_window_load,
-    .unload = prv_window_unload,
-  });
-  const bool animated = true;
-  window_stack_push(s_window, animated);
+static void init(void)
+{
+    s_main_window = window_create();
+    window_set_background_color(s_main_window, GColorBlack);
+
+    window_set_window_handlers(s_main_window, (WindowHandlers){
+                                                  .load = main_window_load,
+                                                  .unload = main_window_unload});
+
+    window_stack_push(s_main_window, true);
+    tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
 }
 
-static void prv_deinit(void) {
-  window_destroy(s_window);
+static void deinit(void)
+{
+    window_destroy(s_main_window);
+    tick_timer_service_unsubscribe();
 }
 
-int main(void) {
-  prv_init();
-
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", s_window);
-
-  app_event_loop();
-  prv_deinit();
+int main(void)
+{
+    init();
+    app_event_loop();
+    deinit();
 }
